c8e7046b0ff5b81d55873de4656ecd0fe4108e4a
Bedoya1905 <santiagobedgo@unisabana.edu.co>
2025-08-20 00:16:02 -0500

    Agregado clase de Game, espero que estos sean los atributos necesarios

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,18 @@
 const offers = document.getElementById("offers");
 const showMoreOffers = document.getElementById("show-more-offers");
 
+// Clase de Game
+// Yo creo que se debe de editar en algun momento en el futuro
+class Game {
+    constructor(id, title, genre, develpoer, boxArt, release, price, offer) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.developer = develpoer;
+        this.boxArt = boxArt;
+        this.release = release;
+        this.price = price;
+        this.offer = offer;
+    }
+}
+
