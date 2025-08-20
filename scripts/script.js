7429d90e0e333c6bb88222a4f7657909d3b92ed3
Bedoya1905 <santiagobedgo@unisabana.edu.co>
2025-08-20 00:27:23 -0500

    Agregado primeros juegos a la libreria

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,11 @@ class Game {
     }
 }
 
+// Esto va a ser epicamente ineficiente, pero por limitantes de mis habilidades y tiempo tocara de esta forma
+// Esta lista contendra toda la libreria de nuestra tienda digital
+
+const library = [
+    new Game(1, "Expedition 33", "RPG", "Sandfall Interactive", "./images/games/ps5/expedition33.jpg", new Date(2025, 3, 24), 50, 0.10),
+    new Game(2, "Collegue Football 26", "Sports", "EA Sports", "./images/games/ps5/colleguefootball.jpg", new Date(2025, 6, 10), 65, 0.10),
+    new Game(3, "Silent Hill 2", "Survival horror", "Konami", "./images/games/ps5/silenthill2.jpg", new Date(2024, 9, 8), 50, 0.4)
+]
