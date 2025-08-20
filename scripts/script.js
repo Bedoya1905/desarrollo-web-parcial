26e131797cd0a85a0d59fe26fc48723b9149a0a3
Bedoya1905 <santiagobedgo@unisabana.edu.co>
2025-08-20 00:38:44 -0500

    Añadido funcion que añade juegos al main (espero que funcione)

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,24 @@ class Game {
 
 const library = [
     new Game(1, "Expedition 33", "RPG", "Sandfall Interactive", "./images/games/ps5/expedition33.jpg", new Date(2025, 3, 24), 50, 0.10),
-    new Game(2, "Collegue Football 26", "Sports", "EA Sports", "./images/games/ps5/colleguefootball.jpg", new Date(2025, 6, 10), 65, 0.10),
+    new Game(2, "Collegue Football 26", "Sports", "EA Sports", "./images/games/ps5/colleguefootball.jpg", new Date(2025, 6, 10), 70, 0.10),
     new Game(3, "Silent Hill 2", "Survival horror", "Konami", "./images/games/ps5/silenthill2.jpg", new Date(2024, 9, 8), 50, 0.4)
-]
+];
+
+// Cambiar esto despues al resto de las cosas
+const setGamesDiscount = (arr = library, docElement) => {
+    docElement.innerHTML += arr.map(
+        // Toca cambiar esto para que aguente lo del cambio en ofertas
+        ({ id, title, release, developer, boxArt, price }) => {
+            return `
+                <div class="game-main" id="${id}">
+                    <h3 class="game-title">${title}</h3>
+                    <p class="game-date">${release.toString()}</p>
+                    <img src="${boxArt}">
+                    <p class="game-price">$${price}</p>
+                    <button class="game-add-button">Add to cart</button>
+                </div>
+            `
+        }
+    )
+}
